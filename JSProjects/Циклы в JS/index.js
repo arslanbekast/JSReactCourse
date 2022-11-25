e547ca28410ad52932637de28e294aedd8efcc03
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-11-25 20:15:07 +0300

    Modified file index.js in Циклы в JS

--- JSProjects/Циклы в JS/index.js
+++ JSProjects/Циклы в JS/index.js
@@ -1,2 +1,22 @@
 "use strict";
 
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+while (num <= 55);
+
+for (let i = 0; i < 8; i++) {
+    
+    if (i === 6) {
+        continue;
+    }
+    console.log(i);
+}
