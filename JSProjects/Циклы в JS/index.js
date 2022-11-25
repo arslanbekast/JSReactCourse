368f7f61cfcae923f80fbdfe75a166af6951c708
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-11-25 20:33:42 +0300

    Modified index.js file in "Циклы в JS"

--- JSProjects/Циклы в JS/index.js
+++ JSProjects/Циклы в JS/index.js
@@ -20,3 +20,41 @@ for (let i = 0; i < 8; i++) {
     }
     console.log(i);
 }
+
+// Цикл в цикле и метки
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+    
+}
+
+let result = '';
+const len = 10;
+
+for (let i = 1; i < len; i++) {
+    
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += '\n';
+    
+}
+
+console.log(result);
+
+// Метки
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level ${j}`);
+        for (let k = 0; k < 3; k++) {
+            // if (k === 2) continue first;
+            if (k === 2) break first;
+            console.log(`Third level ${k}`);           
+        }
+    }   
+}
