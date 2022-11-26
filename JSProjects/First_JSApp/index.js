1f7afdb146f50d867b5287752aa8a80981a44435
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-11-26 16:03:04 +0300

    Modified First_JSApp

--- JSProjects/First_JSApp/index.js
+++ JSProjects/First_JSApp/index.js
@@ -9,12 +9,26 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+    } else {
+        console.log('Error');
+        i--;
+    }  
+}
 
+if (personalMovieDB.count < 10) {
+    console.log("Просотмрено довольно мало фильмов.");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы класический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 console.log(personalMovieDB);
