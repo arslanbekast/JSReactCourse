8ba98d183c8fbfb1bb1d40a830c42719a4fb50a4
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-11-26 17:48:57 +0300

    Modified index.js in "Функции в JS"

--- JSProjects/Функции в JS/index.js
+++ JSProjects/Функции в JS/index.js
@@ -75,3 +75,37 @@ test();
 // Любая функция возвращает undefined, даже если не используется return
 function doNothing() {}
 console.log(doNothing());
+
+// Задачи
+function sayHello(name) {
+    return `Привет, ${name}!`;
+}
+console.log(sayHello('Андрей'));
+
+function returnNeighboringNumbers(num) {
+    return [num-1,num,num+1];
+}
+console.log(returnNeighboringNumbers(10)); 
+
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+console.log( getMathResult(10, 5) ); 
