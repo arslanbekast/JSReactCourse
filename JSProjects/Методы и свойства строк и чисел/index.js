544e14fdc9e4716f6eaa05be025e79fa2ffe62d8
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-11-27 19:38:42 +0300

    Modified index,js in Методы и свойства строк и чисел

--- JSProjects/Методы и свойства строк и чисел/index.js
+++ JSProjects/Методы и свойства строк и чисел/index.js
@@ -1,6 +1,30 @@
 "use strict";
 
+const str = "teSt";
+const arr = [1,2,3];
+
+// Свойство length - кол-во символов строки или кол-во элементов массива
+console.log(str.length);
+console.log(arr.length);
+
+console.log(str[2]);
+
+// Перевод строки в ВЕРХНИЙ регистр
+console.log(str.toUpperCase());
+// Перевод строки в нижний регистр
+console.log(str.toLowerCase());
 
 
+const fruit = "Some fruit";
+// Поиск позиции подстроки в строке
+console.log(fruit.indexOf("q"));
+// Поиск подстроки в строке
+console.log(fruit.slice(5,10));
+console.log(fruit.slice(5));
+console.log(fruit.slice(-5,-1));
+console.log(fruit.substring(5, 10));
+console.log(fruit.substr(5, 5)); //Второй параметр - кол-во сиволов
 
-
+const num = 12.2;
+// Округление
+console.log(Math.round(num));
