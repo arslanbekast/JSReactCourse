9b2ffafe7a561e39c0368f50ff72492bd698c1e3
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-12-02 10:56:36 +0300

    Added "Объекты, деструктуризация объектов (ES6)"

---
+++ JSProjects/Объекты, деструктуризация объектов (ES6)/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const options = {
+    name: "test",
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+options.makeTest();
+// console.log(options.name);
+
+// Удалить свойство объекта
+// delete options.name;
+// console.log(options);
+
+// Перебор объекта
+for (let key in options) {
+
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]})`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }  
+}
+
+console.log(Object.keys(options).length);
+
+
