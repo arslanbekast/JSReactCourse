ffa7e41f5fea599898088ab67e32e3b62d2f19e9
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-12-04 15:28:20 +0300

    Modified tasks.js in Массивы и псевдомассивы

--- JSProjects/Массивы и псевдомассивы/tasks.js
+++ JSProjects/Массивы и псевдомассивы/tasks.js
@@ -21,3 +21,46 @@ function standardizeStrings(arr) {
     });
 }
 standardizeStrings(favoriteCities);
+
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+
+    if (typeof(str) !== 'string') return 'Ошибка';
+
+    let reversedStr = '';
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    return reversedStr;
+}
+
+ console.log( reverse(someString) );
+
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+ function availableCurr(arr, missingCurr) {
+    let str = '';
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+
+    arr.forEach(function(curr, i) {
+        if (curr !== missingCurr) {
+            str += `${curr}\n`;
+        }
+    });
+
+    // Или
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] === missingCurr) {
+    //         continue;
+    //     }
+    //     str += `${arr[i]}\n`;
+    // }
+
+    return str;
+}
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'));
