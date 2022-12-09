f5a6ace3f53504147d5b70b192e820b141a806b1
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-12-09 10:52:15 +0300

    Modified tasks.js in Массивы и псевдомассивы

--- JSProjects/Массивы и псевдомассивы/tasks.js
+++ JSProjects/Массивы и псевдомассивы/tasks.js
@@ -64,3 +64,25 @@ const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
     return str;
 }
 console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'));
+
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Sara'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
+}
+console.log( sortStudentsByGroups(students) );
