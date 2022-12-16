236f2dbd11b57040bc44d3f160894de4eb18c9a6
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-12-16 21:41:06 +0300

    Added tasks.js to "Рекурсия в JS"

---
+++ JSProjects/Рекурсия в JS/tasks.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function factorial(n) {
+    if (typeof(n) !== 'number' || !Number.isInteger(n)) {
+        return "Ошибка, проверьте данные";
+    }
+
+    if (n >= 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
+
+    // Более короткий вариант, который вы можете встретить
+    // Но не учитывает отрицательные значения
+    return n ? n * factorial(n - 1) : 1;
+}
+
+console.log(factorial(4));
