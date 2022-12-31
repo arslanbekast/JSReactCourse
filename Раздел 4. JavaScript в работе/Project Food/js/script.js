4d42f248deda71ddfc854d3451d0fcf73a940904
Arslanbek Astemirov <s-o-f-t@mail.ru>
2022-12-31 18:48:12 +0300

    Handling elapsed date on a timer in a Food project

--- Раздел 4. JavaScript в работе/Project Food/js/script.js
+++ Раздел 4. JavaScript в работе/Project Food/js/script.js
@@ -46,11 +46,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const deadline = '2023-01-12';
 
     function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds =  Math.floor((t / 1000) % 60);
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor( t / (1000 * 60 * 60 * 24) );
+            hours = Math.floor( (t / (1000 * 60 * 60)) % 24 );
+            minutes = Math.floor( (t / 1000 / 60) % 60 );
+            seconds =  Math.floor( (t / 1000) % 60 );
+        }
+              
 
         return {
             'total': t,
