7088af113d78a408fe14db96d506f972c76a293d
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-01-02 18:53:04 +0300

    Added "Функции-конструкторы в JS"

---
+++ Раздел 4. JavaScript в работе/Функции-конструкторы в JS/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Функции-конструкторы в JS
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+User.prototype.exit = function() {
+    console.log(`Пользователь ${this.name} ушел`);
+};
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.hello();
+ivan.exit();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
