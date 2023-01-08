2b640915cfab4399d4ad74c5ad0c2b5510523f9b
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-01-08 20:22:32 +0300

    Implemented the functionality of receiving data from the server using the Axios library for menu cards in the Food project.

--- Раздел 4. JavaScript в работе/Project Food/js/script.js
+++ Раздел 4. JavaScript в работе/Project Food/js/script.js
@@ -203,9 +203,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         return await res.json();
     };
-    getResources('http://localhost:3000/menu')
+    // getResources('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
