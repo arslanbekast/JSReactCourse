79d6d8d0040bce08c42dcf7f909f66c24139633c
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-01-20 16:19:13 +0300

    Added "ES6 Modules"

---
+++ Раздел 5. Продвинутый JavaScript/ES6 Modules/src/js/main.js
@@ -0,0 +1,13 @@
+export let one = 1;
+
+let two = 2;
+
+export {two};
+
+// export function sayHi() {
+//     console.log('Hello!');
+// }
+
+export default function sayHi() {
+    console.log('Hello!');
+}
---
+++ Раздел 5. Продвинутый JavaScript/ES6 Modules/src/js/script.js
@@ -0,0 +1,13 @@
+import {one, two} from './main';
+// import {one as first} from './main';
+// import * as data from './main';
+import sayHi from './main';
+
+console.log(`${one} and ${two}`);
+// console.log(first);
+// console.log(`${data.one} and ${data.two}`);
+// data.sayHi();
+sayHi();
+
+
+
