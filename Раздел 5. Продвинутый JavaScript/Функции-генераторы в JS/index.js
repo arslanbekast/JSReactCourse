72d347555b372f6e81a6980d00734d6b14331aac
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-01-23 21:39:16 +0300

    Added "Функции-генераторы в JS"

---
+++ Раздел 5. Продвинутый JavaScript/Функции-генераторы в JS/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// Функции-генераторы в JS
+// Генераторы являются функциями с возможностью выхода и последующего входа. 
+// Их контекст исполнения (значения переменных) сохраняется при последующих входах.
+
+function* generator() {
+    yield 'S';
+    yield 'c';
+    yield 'r';
+    yield 'i';
+    yield 'p';
+    yield 't';
+}
+
+const str = generator();
+
+console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+console.log(str.next());
+
+function* count(n) {
+    for (let i = 0; i < n; i++) {
+        yield i;
+    }
+}
+
+// const counter = count(7);
+
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+// console.log(counter.next().value);
+
+for (let k of count(7)) {
+    console.log(k);
+}
