b3e56fed3aa6b98020e3ee324bdbbe50e9d66662
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-02-09 19:15:47 +0300

    Changes in my-app

--- Раздел 6. Библиотека React. Базовый уровень/Create React App - создаем свое приложение/my-app/src/index.js
+++ Раздел 6. Библиотека React. Базовый уровень/Create React App - создаем свое приложение/my-app/src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const text = 'Hello world';
+const elem = (
+  <div>
+    <h2 className='text'>{text}</h2>
+    <label htmlFor="text">Введите что-нибудь</label>
+    <input type="text" id='text'/>
+    <button tabIndex={0}>Button</button>
+  </div>
+  
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
