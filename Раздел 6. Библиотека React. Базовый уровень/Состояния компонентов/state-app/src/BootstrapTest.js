8de0e054b79a32af71f96c9e4bbbb5c58680424b
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-02-18 14:30:46 +0300

    Used react-bootstrap in state-app

---
+++ Раздел 6. Библиотека React. Базовый уровень/Состояния компонентов/state-app/src/BootstrapTest.js
@@ -0,0 +1,77 @@
+import {Container, Row, Col, Carousel, Form, Button} from 'react-bootstrap';
+
+const BootstrapTest = () => {
+    return (
+        <Container className='mt-5 mb-5'>
+        <Row>
+            <Col>
+            
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            
+            </Col>
+            <Col>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/16:9/w_2123,h_1194,c_limit/phonepicutres-TA.jpg"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://wallpapers.com/images/featured/en3dnh2zi84sgt3t.jpg"
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://www.planetware.com/wpimages/2019/11/canada-in-pictures-beautiful-places-to-photograph-morraine-lake.jpg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            </Col>
+        </Row>
+        </Container>
+    );
+};
+
+export default BootstrapTest;
--- Раздел 6. Библиотека React. Базовый уровень/Состояния компонентов/state-app/src/index.js
+++ Раздел 6. Библиотека React. Базовый уровень/Состояния компонентов/state-app/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Button} from './App';
+import BootstrapTest from './BootstrapTest';
 import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BigButton = styled(Button)`
   margin: 0 auto;
@@ -16,6 +18,7 @@ root.render(
   <React.StrictMode>
     <App />
     <BigButton as="a">Отправить отчет</BigButton>
+    <BootstrapTest/>
   </React.StrictMode>
 );
 
