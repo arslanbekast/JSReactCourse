7dae552cc8287b443205fe6bebf848787f47bd58
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-02-18 14:04:21 +0300

    Used styled-components in state-app

--- Раздел 6. Библиотека React. Базовый уровень/Состояния компонентов/state-app/src/App.js
+++ Раздел 6. Библиотека React. Базовый уровень/Состояния компонентов/state-app/src/App.js
@@ -1,6 +1,35 @@
 import { Component } from 'react';
+import styled from 'styled-components';
 
 import './App.css';
+
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0,.2);
+  a {
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${props => props.active ? 'orange' : 'black'};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+const Header = styled.h2`
+  font-size: 22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0,0,0, .2);
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+`;
 
 class WhoAmI extends Component {
   constructor(props) {
@@ -29,29 +58,34 @@ class WhoAmI extends Component {
     const {name, surname, link} = this.props;
     const {years, position} = this.state;
     return (
-      <div>
-        <button onClick={this.nextYear}>+++</button>
-        <h1>My name is {name}, 
+      <EmpItem active>
+        <Button onClick={this.nextYear}>+++</Button>
+        <Header>My name is {name}, 
             surname - {surname}, 
             age - {years}, 
-            position - {position}</h1>
+            position - {position}</Header>
         <a href={link}>My profile</a>
         <form>
           <span>Введите должность</span>
           <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
         </form>
-      </div>
+      </EmpItem>
     );
   }
   
 }
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <WhoAmI name='John' surname="Smith" link="facebook.com"/>
       <WhoAmI name='Alex' surname="Shepard" link="vk.com"/>
-    </div>
+    </Wrapper>
   );
 }
 
--- Раздел 6. Библиотека React. Базовый уровень/Состояния компонентов/state-app/src/index.js
+++ Раздел 6. Библиотека React. Базовый уровень/Состояния компонентов/state-app/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Button} from './App';
+import styled from 'styled-components';
+
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 250px;
+  text-align: center;
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <BigButton as="a">Отправить отчет</BigButton>
   </React.StrictMode>
 );
 
