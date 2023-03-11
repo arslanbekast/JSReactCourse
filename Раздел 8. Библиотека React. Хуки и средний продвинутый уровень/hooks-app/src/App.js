ee477889df155b943b9207745e0ee66d84dc4355
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-03-11 18:15:33 +0300

    Used the useCallback hook in hooks-app

--- Раздел 8. Библиотека React. Хуки и средний продвинутый уровень/hooks-app/src/App.js
+++ Раздел 8. Библиотека React. Хуки и средний продвинутый уровень/hooks-app/src/App.js
@@ -1,59 +1,14 @@
-import {Component, useEffect, useState} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
-// class Slider extends Component {
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
-//         }
-//     }
-
-        // componentDidMount() {
-        //     document.title = `Slide ${this.state.slide}`
-        // }
-
-        // componentDidUpdate() {
-        //     document.title = `Slide ${this.state.slide}`
-        // }
-
-//     changeSlide = (i) => {
-//         this.setState(({slide}) => ({
-//             slide: slide + i
-//         }))
-//     }
-
-//     toggleAutoplay = () => {
-//         this.setState(({autoplay}) => ({
-//             autoplay: !autoplay
-//         }))
-//     }
-
-//     render() {
-//         return (
-//             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                     </div>
-//                 </div>
-//             </Container>
-//         )
-//     }
+// const getSomeImages = () => {
+//     console.log('fetching');
+//     return [
+//         "https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg",
+//         "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg"
+//     ]
 // }
-
 
 const Slider = (props) => {
 
@@ -68,6 +23,14 @@ const Slider = (props) => {
         console.log('Autoplay');
     }, [autoplay]);
 
+    const getSomeImages = useCallback(() => {
+        console.log('fetching');
+        return [
+            "https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg",
+            "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg"
+        ]
+    }, [slide]);
+
     function changeSlide(i) {
         setSlide(slide => slide + i);
     }
@@ -79,7 +42,16 @@ const Slider = (props) => {
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                {/* {
+                    getSomeImages().map((url, i) => {
+                        return (
+                            <img key={i} className="d-block w-100" src={url} alt="slide" />
+                        )
+                    })
+                } */}
+
+                <Slide getSomeImages={getSomeImages}/>
+                
                 <div className="text-center mt-5">Active slide {slide} <br/> 
                     {autoplay ? 'auto' : null}
                 </div>
@@ -99,6 +71,19 @@ const Slider = (props) => {
     )
 }
 
+const Slide = ({getSomeImages}) => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        setImages(getSomeImages())
+    }, [getSomeImages]);
+
+    return (
+        <>
+            {images.map((url, i) => <img key={i} className="d-block w-100" src={url} alt="slide" />)}
+        </>
+    )
+}
 
 function App() {
   return (
