82cdc84ab63318c121062a65b03d4f093b45f4e7
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-03-12 15:06:05 +0300

    Used the useRef hook in ref-app

--- Раздел 7. Библиотека React. Средний фундаментальный уровень/Что такое ref и зачем он нужен/ref-app/src/App.js
+++ Раздел 7. Библиотека React. Средний фундаментальный уровень/Что такое ref и зачем он нужен/ref-app/src/App.js
@@ -1,38 +1,30 @@
-import React, {Component} from 'react';
+import {useRef, useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-class Form extends Component {
-    // myRef = React.createRef();
+const Form = () => {
+    const [text, setText] = useState('');
 
-    // componentDidMount() {
-    //     this.myRef.current.focus();
-    // }
+    const myRef = useRef(1);
 
-    setInputRef = elem => {
-        this.myRef = elem
-    }
+    useEffect(() => {
+        myRef.current = text;
+    });
 
-    focusFirstTI = () => {
-        if (this.myRef) this.myRef.focus();
-    }
-
-    render() {
-        return (
-            <Container>
-                <form className="w-50 border mt-5 p-3 m-auto">
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input ref={this.setInputRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea onClick={this.focusFirstTI} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                </form>
-            </Container>
-        )
-    }
+    return (
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea value={myRef.current} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </Container>
+    )
 }
 
 function App() {
