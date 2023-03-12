8b47641eb853ae84f7765e998c7651926872c678
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-03-12 14:43:00 +0300

    Used the useMemo hook in hooks-app

--- Раздел 8. Библиотека React. Хуки и средний продвинутый уровень/hooks-app/src/App.js
+++ Раздел 8. Библиотека React. Хуки и средний продвинутый уровень/hooks-app/src/App.js
@@ -1,14 +1,11 @@
-import {useEffect, useState, useCallback} from 'react';
+import {useEffect, useState, useCallback, useMemo} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-// const getSomeImages = () => {
-//     console.log('fetching');
-//     return [
-//         "https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg",
-//         "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg"
-//     ]
-// }
+const countTotal = (num) => {
+    console.log('counting...');
+    return num + 10;
+}
 
 const Slider = (props) => {
 
@@ -39,22 +36,30 @@ const Slider = (props) => {
         setAutoplay(!autoplay);
     }
 
+    const total = useMemo(() => {
+        return countTotal(slide);
+    }, [slide]);
+
+    const style = useMemo(() => {
+        return {
+            color: slide > 4 ? 'red' : 'black'
+        }
+    }, [slide]);
+
+    useEffect(() => {
+        console.log('style');
+    }, [style])
+
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                {/* {
-                    getSomeImages().map((url, i) => {
-                        return (
-                            <img key={i} className="d-block w-100" src={url} alt="slide" />
-                        )
-                    })
-                } */}
-
+              
                 <Slide getSomeImages={getSomeImages}/>
                 
                 <div className="text-center mt-5">Active slide {slide} <br/> 
                     {autoplay ? 'auto' : null}
                 </div>
+                <div style={style} className="text-center mt-5">Total slides: {total}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
