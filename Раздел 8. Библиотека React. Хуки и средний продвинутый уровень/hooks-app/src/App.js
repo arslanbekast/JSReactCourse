9506920c03efbcabf3183fe5d903be5fea7ab760
Arslanbek Astemirov <astemirow@mail.ru>
2023-04-27 12:10:48 +0300

    Used the useReducer hook in hooks-app

--- Раздел 8. Библиотека React. Хуки и средний продвинутый уровень/hooks-app/src/App.js
+++ Раздел 8. Библиотека React. Хуки и средний продвинутый уровень/hooks-app/src/App.js
@@ -1,65 +1,40 @@
-import {useEffect, useState, useCallback, useMemo} from 'react';
+import {useState, useReducer} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-const countTotal = (num) => {
-    console.log('counting...');
-    return num + 10;
+function reducer(state, action) {
+    switch (action.type) {
+        case 'toggle':
+            return {autoplay: !state.autoplay}
+        case 'slow':
+            return {autoplay: 300}
+        case 'fast':
+            return {autoplay: 700}
+        case 'custom':
+            return {autoplay: action.payload}
+        default:
+            throw new Error();
+    }
 }
 
-const Slider = (props) => {
+function init(initial) {
+    return {autoplay: initial}
+}
 
+const Slider = ({initial}) => {
     const [slide, setSlide] = useState(0);
-    const [autoplay, setAutoplay] = useState(false);
-
-    useEffect(() => {
-        document.title = `Slide ${slide}`;
-    }, [slide]);
-
-    useEffect(() => {
-        console.log('Autoplay');
-    }, [autoplay]);
-
-    const getSomeImages = useCallback(() => {
-        console.log('fetching');
-        return [
-            "https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg",
-            "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg"
-        ]
-    }, [slide]);
+    // const [autoplay, setAutoplay] = useState(false);
+    const [autoplay, dispatch] = useReducer(reducer,initial, init);
 
     function changeSlide(i) {
         setSlide(slide => slide + i);
     }
 
-    function toggleAutoplay() {
-        setAutoplay(!autoplay);
-    }
-
-    const total = useMemo(() => {
-        return countTotal(slide);
-    }, [slide]);
-
-    const style = useMemo(() => {
-        return {
-            color: slide > 4 ? 'red' : 'black'
-        }
-    }, [slide]);
-
-    useEffect(() => {
-        console.log('style');
-    }, [style])
-
     return (
         <Container>
             <div className="slider w-50 m-auto">
-              
-                <Slide getSomeImages={getSomeImages}/>
-                
-                <div className="text-center mt-5">Active slide {slide} <br/> 
-                    {autoplay ? 'auto' : null}
-                </div>
-                <div style={style} className="text-center mt-5">Total slides: {total}</div>
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay.autoplay ? 'auto' : null} </div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
@@ -69,31 +44,26 @@ const Slider = (props) => {
                         onClick={() => changeSlide(1)}>+1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={toggleAutoplay}>toggle autoplay</button>
+                        onClick={() => dispatch({type: 'toggle'})}>toggle autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'slow'})}>slow autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'fast'})}>fast autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={(e) => dispatch({type: 'custom', payload: +e.target.textContent})}>1000</button>
                 </div>
             </div>
         </Container>
     )
 }
 
-const Slide = ({getSomeImages}) => {
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        setImages(getSomeImages())
-    }, [getSomeImages]);
-
-    return (
-        <>
-            {images.map((url, i) => <img key={i} className="d-block w-100" src={url} alt="slide" />)}
-        </>
-    )
-}
-
 function App() {
-  return (
-        <Slider/>
-  );
+    return (
+        <Slider initial={false}/>
+    );
 }
 
 export default App;
