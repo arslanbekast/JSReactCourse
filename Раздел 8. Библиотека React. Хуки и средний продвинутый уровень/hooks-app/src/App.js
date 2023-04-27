9f39c834b4e7a8d5bd2b4fce522561c8590d7244
Arslanbek Astemirov <astemirow@mail.ru>
2023-04-27 15:34:50 +0300

    Used Higher Order Components (HOC) in hooks-app

--- Раздел 8. Библиотека React. Хуки и средний продвинутый уровень/hooks-app/src/App.js
+++ Раздел 8. Библиотека React. Хуки и средний продвинутый уровень/hooks-app/src/App.js
@@ -1,68 +1,84 @@
-import {useState, useReducer} from 'react';
+import {useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-function reducer(state, action) {
-    switch (action.type) {
-        case 'toggle':
-            return {autoplay: !state.autoplay}
-        case 'slow':
-            return {autoplay: 300}
-        case 'fast':
-            return {autoplay: 700}
-        case 'custom':
-            return {autoplay: action.payload}
-        default:
-            throw new Error();
+const withSlider = (BaseComponent, getData) => {
+    return (props) => {
+        const [slide, setSlide] = useState(0);
+        const [autoplay, setAutoplay] = useState(false)
+
+        useEffect(() => {
+            setSlide(getData());
+        }, [])
+
+        function changeSlide(i) {
+            setSlide(slide => slide + i);
+        }
+
+        return <BaseComponent 
+                    {...props}
+                    slide={slide} 
+                    autoplay={autoplay} 
+                    changeSlide={changeSlide}
+                    setAutoplay={setAutoplay}/>
     }
 }
 
-function init(initial) {
-    return {autoplay: initial}
-}
+const getDataFromFirstFetch = () => {return 10};
+const getDataFromSecondFetch = () => {return 20};
 
-const Slider = ({initial}) => {
-    const [slide, setSlide] = useState(0);
-    // const [autoplay, setAutoplay] = useState(false);
-    const [autoplay, dispatch] = useReducer(reducer,initial, init);
-
-    function changeSlide(i) {
-        setSlide(slide => slide + i);
-    }
+const SliderFirst = (props) => {
 
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay.autoplay ? 'auto' : null} </div>
+                <div className="text-center mt-5">Active slide {props.slide}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}>-1</button>
+                        onClick={() => props.changeSlide(-1)}>-1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}>+1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => dispatch({type: 'toggle'})}>toggle autoplay</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => dispatch({type: 'slow'})}>slow autoplay</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => dispatch({type: 'fast'})}>fast autoplay</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={(e) => dispatch({type: 'custom', payload: +e.target.textContent})}>1000</button>
+                        onClick={() => props.changeSlide(1)}>+1</button>
                 </div>
             </div>
         </Container>
     )
 }
 
+const SliderSecond = (props) => {
+
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {props.slide} <br/>{props.autoplay ? 'auto' : null} </div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.setAutoplay(autoplay => !props.autoplay)}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+const SliderWithFirstFetch = withSlider(SliderFirst, getDataFromFirstFetch);
+const SliderWithSecondFetch = withSlider(SliderSecond, getDataFromSecondFetch);
+
 function App() {
     return (
-        <Slider initial={false}/>
+        <>
+            <SliderWithFirstFetch />
+            <SliderWithSecondFetch />
+        </>
     );
 }
 
