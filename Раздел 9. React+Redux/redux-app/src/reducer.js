6c132e74539f7820e89453afbc2893c7e2fa3f12
Arslanbek Astemirov <astemirow@mail.ru>
2023-05-12 10:27:02 +0300

    Connected Redux to React using the useSelector() hook in redux-app

--- Раздел 9. React+Redux/redux-app/src/reducer.js
+++ Раздел 9. React+Redux/redux-app/src/reducer.js
@@ -1,4 +1,4 @@
-const initialState = {value: 0};
+const initialState = {counter: 0};
 
 const reducer = (state = initialState, action) => {
 
@@ -6,17 +6,17 @@ const reducer = (state = initialState, action) => {
     case 'INC':
       return {
         ...state,
-        value: state.value + 1
+        counter: state.counter + 1
       };
     case 'DEC':
       return {
         ...state,
-        value: state.value - 1
+        counter: state.counter - 1
       };
     case 'RND':
       return {
         ...state,
-        value: state.value * action.payload
+        counter: state.counter * action.payload
       };
   
     default:
