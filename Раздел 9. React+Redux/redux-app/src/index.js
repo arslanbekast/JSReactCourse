6989c6260ffeddf0c584b52bc7b5bd1d618c7310
Arslanbek Astemirov <astemirow@mail.ru>
2023-07-22 16:56:25 +0300

    Commit

--- Раздел 9. React+Redux/redux-app/src/index.js
+++ Раздел 9. React+Redux/redux-app/src/index.js
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux';
 
 import App from './components/App';
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
